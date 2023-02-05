a7c80f9f6c7445428292208f62c1f45b01a08c69
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-02-05 11:25:56 +0100

    feat: return a list

--- src/main.rs
+++ src/main.rs
@@ -26,15 +26,25 @@ impl nu_plugin::Plugin for NuPlugin {
         call: &EvaluatedCall,
         _input: &Value,
     ) -> Result<Value, LabeledError> {
-        Ok(Value::Nothing { span: call.head })
+        Ok(Value::List {
+            vals: Vec::new(),
+            span: call.head,
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
     use nu_plugin::Plugin;
+    use nu_protocol::Span;
 
     use super::*;
+
+    const SIMPLE_CALL: EvaluatedCall = EvaluatedCall {
+        head: Span::unknown(),
+        positional: Vec::new(),
+        named: Vec::new(),
+    };
 
     #[fixture]
     fn plugin() -> impl Plugin {
@@ -42,10 +52,26 @@ mod tests {
     }
 
     #[rstest]
-    fn should_have_from_beancount_signature(plugin: impl Plugin) {
+    fn should_have_from_beancount_command(plugin: impl Plugin) {
         assert!(plugin
             .signature()
             .into_iter()
             .any(|s| &s.name == "from beancount"));
+    }
+
+    #[rstest]
+    fn should_return_empty_list_for_empty_input(mut plugin: impl Plugin) {
+        let result = plugin.run(
+            "from beancount",
+            &SIMPLE_CALL,
+            &Value::String {
+                val: "".into(),
+                span: Span::unknown(),
+            },
+        );
+        let Ok(Value::List { vals, .. }) = result else {
+            panic!("Expected a list value but was: {result:?}");
+        };
+        assert!(vals.is_empty());
     }
 }
